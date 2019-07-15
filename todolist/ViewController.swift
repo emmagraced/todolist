61acc5deea761fefb9f2bb98df44057abcbd2844
Emma Davis <emmadavis@Emmas-MacBook-Air.local>
2019-07-15 09:39:00 -0700

    initial commit

---
+++ todolist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todolist
+//
+//  Created by Emma Davis on 7/15/19.
+//  Copyright © 2019 Emma Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
